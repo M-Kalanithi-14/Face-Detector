50b152048e1214b17c81dc11b89e88b0ebf3e767
Programmin-in-Python <alfaromeobugatti@gmail.com>
2021-05-23 10:13:41 +0530

    Update FaceDetector.py

--- FaceDetector.py
+++ FaceDetector.py
@@ -5,8 +5,7 @@ simplefilter("ignore")
 
 TrainedFaceData =  cv2.CascadeClassifier("haarcascades_frontalfaces.xml")
 
-""" #This is For a Static Single Image Detection
-
+#This is For a Static Single Image Detection
 img = cv2.imread("assets/sample.png")
 GrayScaleImg = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
@@ -16,11 +15,12 @@ for (x, y, w, h) in face_coor:
 	cv2.rectangle(img, (x, y), (x+w,  y+h), (0, 0, 255), 5)
 
 cv2.imshow("Face Detected", img)
+# Press any Key to Proceed the Execution
 cv2.waitKey()
-"""
+
 
 # This is for a Video esp. from a WebCam
-webcam = cv2.VideoCapture("assets/Avengers.mp4")
+webcam = cv2.VideoCapture(0)
 
 while True:
 	SucessfulFrameRead, frame = webcam.read()	
@@ -35,7 +35,7 @@ while True:
 	cv2.imshow("Face Detected", frame)
 	key = cv2.waitKey(1)
 
-	# Press Q to Quit
+	# Press 'Q' or 'q' to Quit
 	if key in (81, 113):
 		break
 
